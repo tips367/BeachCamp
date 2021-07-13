e31ed56e2bd9ee4cc1200cf8da3de75bb077b19f
tips367 <tipu367.khan@hotmail.com>
2021-07-13 21:02:16 +0530

    campgrounds new and create

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const { urlencoded } = require('express');
 
 mongoose.connect('mongodb://localhost:27017/beach-camp', {
 	useNewUrlParser: true,
@@ -20,6 +21,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 	res.send('Hi from BeachCamp');
 });
@@ -27,6 +30,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', async (req, res) => {
+	res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
