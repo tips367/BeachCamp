50e5caee3cd028c20786637c41f9cce7fd841dff
tips367 <tipu367.khan@hotmail.com>
2021-07-14 22:20:53 +0530

    adding random images from unsplash api

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 187; i++) {
 		const random = Math.floor(Math.random() * 187);
+		const price = Math.floor(Math.random() * 1200);
 		const camp = new Campground({
 			location: `${cities[random].city}, ${cities[random].admin_name}`,
-			title: `${sample(descriptors)} ${sample(places)}`
+			title: `${sample(descriptors)} ${sample(places)}`,
+			image: 'https://source.unsplash.com/collection/483251/',
+			description:
+				'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos eaque temporibus, veniam eos veritatis qui! Exercitationem eos similique velit repudiandae ex totam expedita doloremque ullam, inventore accusamus quia atque voluptatum.',
+			price
 		});
 		await camp.save();
 	}
