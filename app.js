3ceefcc4f289eee5aed0fa996211efada9c4f63f
tips367 <tipu367.khan@hotmail.com>
2021-07-24 02:11:02 +0530

    creating reviews

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const { urlencoded } = require('express');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/beach-camp', {
 	useNewUrlParser: true,
@@ -98,6 +99,18 @@ app.delete(
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
 		res.redirect('/campgrounds/');
+	})
+);
+
+app.post(
+	'/campgrounds/:id/reviews',
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
 	})
 );
 
