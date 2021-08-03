501dab42f72cb861954359742ee4056e6121a0c2
tips367 <tipu367.khan@hotmail.com>
2021-08-03 21:28:06 +0530

    fixing register route

--- routes/users.js
+++ routes/users.js
@@ -13,13 +13,16 @@ router.get(
 
 router.post(
 	'/register',
-	catchAsync(async (req, res) => {
+	catchAsync(async (req, res, next) => {
 		try {
 			const { email, username, password } = req.body;
 			const user = new User({ email, username });
 			const registeredUser = await User.register(user, password);
-			req.flash('success', 'Welcome to BeachCamp!');
-			res.redirect('/campgrounds');
+			req.login(registeredUser, (err) => {
+				if (err) return next(err);
+				req.flash('success', 'Welcome to BeachCamp!');
+				res.redirect('/campgrounds');
+			});
 		} catch (e) {
 			req.flash('error', e.message);
 			res.redirect('/register');
