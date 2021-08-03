bb63ac0e1ca3c5b324bf1ef8bc87cdebb3110959
tips367 <tipu367.khan@hotmail.com>
2021-08-03 21:37:24 +0530

    returnTo original page after logging in

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,9 @@ router.post(
 	passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
 	catchAsync(async (req, res) => {
 		req.flash('success', 'Welcome back!');
-		res.redirect('/campgrounds');
+		const redirectUrl = req.session.returnTo || '/campgrounds';
+		delete req.session.returnTo;
+		res.redirect(redirectUrl);
 	})
 );
 
