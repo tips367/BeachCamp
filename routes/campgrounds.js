a3775824a3e5ef8c5192d2d3ece2bd4048fc854e
tips367 <tipu367.khan@hotmail.com>
2021-08-06 21:22:32 +0530

    store uploaded image links in mongo

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,7 +11,7 @@ const upload = multer({ storage });
 router
 	.route('/')
 	.get(catchAsync(campgrounds.index))
-	.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+	.post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
