787999ccc4ff4718ca63a6c6747a900779487830
tips367 <tipu367.khan@hotmail.com>
2021-08-11 23:45:42 +0530

    centering the map on a campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v11',
-	center: [ -74.5, 40 ],
-	zoom: 9
+	center: campground.geometry.coordinates,
+	zoom: 10
 });
+
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
