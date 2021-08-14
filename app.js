cbf055cb8745fca145dade8891db0b8e18774627
tips367 <tipu367.khan@hotmail.com>
2021-08-15 00:13:26 +0530

    content security policy

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const { urlencoded } = require('express');
 
@@ -42,6 +44,49 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
+app.use(helmet({ contentSecurityPolicy: false }));
+
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com',
+	'https://api.tiles.mapbox.com',
+	'https://api.mapbox.com',
+	'https://kit.fontawesome.com',
+	'https://cdnjs.cloudflare.com',
+	'https://cdn.jsdelivr.net'
+];
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com',
+	'https://stackpath.bootstrapcdn.com',
+	'https://api.mapbox.com',
+	'https://api.tiles.mapbox.com',
+	'https://fonts.googleapis.com',
+	'https://use.fontawesome.com',
+	'https://cdn.jsdelivr.net'
+];
+const connectSrcUrls = [ 'https://api.mapbox.com', 'https://*.tiles.mapbox.com', 'https://events.mapbox.com' ];
+const fontSrcUrls = [];
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: [ "'self'", ...connectSrcUrls ],
+			scriptSrc: [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+			styleSrc: [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+			workerSrc: [ "'self'", 'blob:' ],
+			childSrc: [ 'blob:' ],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/davodacdr/',
+				'https://images.unsplash.com'
+			],
+			fontSrc: [ "'self'", ...fontSrcUrls ]
+		}
+	})
+);
 
 const sessionConfig = {
 	secret: 'dummy',
